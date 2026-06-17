b8797fe3fef62d4f0dfd62862bc24ae2cef58979
Mikolaj Gucki <24221859+mikolajgucki@users.noreply.github.com>
2026-06-17 15:51:19 +0200

    feat(): remove the basic evaluator

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,7 +2,6 @@ export * from './math/index.js';
 export * from './errors/index.js';
 export * from './string/index.js';
 export * from './model-provider/index.js';
-export * from './evaluator/index.js';
 export * from './llm/index.js';
 export * from './llm-as-a-judge/index.js';
 export * from './llm-experimentation/index.js';
